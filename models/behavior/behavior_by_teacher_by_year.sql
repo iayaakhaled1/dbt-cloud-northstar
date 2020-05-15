00e5cf5b2063668bedd2d6b3a95e3b1c5491aff3
Kyle Coapman <kyle.d.coapman@gmail.com>
2020-05-15 20:50:03 +0000

    Updated Teacher and Student Behavior Tables

--- models/behavior/behavior_by_teacher_by_year.sql
+++ models/behavior/behavior_by_teacher_by_year.sql
@@ -4,36 +4,50 @@ years as (select * from {{ ref('academic_years')}}),
 
 pos_events as (
     select 
-        events.student_id,
-        events.firstlast,
+        events.teacher_name,
         years.year,
-        sum(merit_value) as positive_total
+        sum(merit_value) as positive_sum,
+        count(merit_value) as positive_count
     from events
     left join years
     on (events.timestamp >= years.start_date and events.timestamp <= years.end_date)
     where events.behavior_category = 'Positive Behaviors'
-    group by 1, 2, 3
+    group by 1, 2
     order by 1
 ),
 
 neg_events as (
     select 
-        events.student_id,
-        events.firstlast,
+        events.teacher_name,
         years.year,
-        sum(merit_value) as positive_total
+        sum(merit_value) as negative_sum,
+        count(merit_value) as negative_count
     from events
     left join years
     on (events.timestamp >= years.start_date and events.timestamp <= years.end_date)
     where events.behavior_category = 'Negative Behaviors'
-    group by 1, 2, 3
+    group by 1, 2
     order by 1
 ),
 
 final as (
-    select pos_event
+    select 
+        pos_events.teacher_name,
+        pos_events.year,
+        pos_events.positive_sum,
+        neg_events.negative_sum,
+        pos_events.positive_count,
+        neg_events.negative_count,
+        pos_events.positive_count + neg_events.negative_count as events,
+        pos_events.positive_count / neg_events.negative_count as ratio
+
+    from pos_events
+
+    left join neg_events
+
+    on (pos_events.teacher_name = neg_events.teacher_name and pos_events.year = neg_events.year)
+    
+    order by 1, 2
 )
 
-
-
-select * from pos_events
+select * from final
