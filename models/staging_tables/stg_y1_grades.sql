6476a4f2b1848ba0803a51847107ee85a92722e7
Aya khaled <ayaakhaled.data@gmail.com>
2024-01-21 14:47:07 +0000

    new

--- models/staging_tables/stg_y1_grades.sql
+++
@@ -1,7 +0,0 @@
-select 
-    "STUDENT NUMBER" as student_id,
-    SECTIONID as section_id,
-    FINALGRADENAME as quarter,
-    PERCENT as pct
-
-from {{ source('powerschool','y1_grades_updated') }}
